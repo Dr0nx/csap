e7c687daad0c2b02b274eab39ea5ce0f8cc4b7b6
Andrey Bozhkov <kovbozh@gmail.com>
2022-02-06 18:07:47 +0300

    add task_5.py

---
+++ lesson_1/task_5.py
@@ -0,0 +1,13 @@
+""" Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в строковый тип на
+кириллице. """
+from platform import system
+from subprocess import Popen, PIPE
+
+# param = '-n' if system().lower() == 'windows' else '-c'
+# for host in 'yandex.ru', 'youtube.com':
+#     args = ['ping', param, '1', host]
+#     result = Popen(args, stdout=PIPE)
+#     for line in result.stdout:
+#         print(line)
+#         line = line.decode('cp866').encode('utf-8')
+#         print(line.decode('utf-8'))
