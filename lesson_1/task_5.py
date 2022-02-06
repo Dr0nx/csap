8bd6ca76883b05459a26f8fa8943739bcb5b9d8b
Dr0n <kovbozh@gmail.com>
2022-02-06 21:02:08 +0300

    edit task_5.py

--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -2,14 +2,15 @@
 кириллице. """
 from platform import system
 from subprocess import Popen, PIPE
+# from
 
+print('Начало')
 param = '-n' if system().lower() == 'windows' else '-c'
 for host in 'yandex.ru', 'youtube.com':
     args = ['ping', param, '1', host]
     result = Popen(args, stdout=PIPE)
     for line in result.stdout:
-        print(line)
-
-        print(line.decode('utf-8').encode('windows-1251'))
+        print(line.decode('cp1251').encode('utf8'))
+        # print(line.decode('utf-8').encode('cp1251'))
         # line = line.decode('utf-8').encode('windows-1251')
         # print(line)
