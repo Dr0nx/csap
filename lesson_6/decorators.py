3149e312161a1b8cf1e8edb7ea09492c799fad55
Andrey Bozhkov <kovbozh@gmail.com>
2022-02-24 19:57:30 +0300

    edit decorators.py

--- lesson_6/decorators.py
+++ lesson_6/decorators.py
@@ -18,9 +18,9 @@ def log(func_to_log):
         result = func_to_log(*args, **kwargs)
         LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}.'
                      f' Вызов из модуля {func_to_log.__module__}.'
-                     f' Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}.'
-                     f' Вызов из функции {inspect.stack()[1][3]}.'
-                     f' @@@Вызов из функции {sys._getframe().f_back.f_code.co_name}.'
+                     # f' Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}.'
+                     # f' Вызов из функции {inspect.stack()[1][3]}.'
+                     # f' @@@Вызов из функции {sys._getframe().f_back.f_code.co_name}.'
                      f' @@@Вызов из модуля {sys._getframe().f_back.f_code.co_filename.split("/")[-1]}.')
         return result
 
