5f82a59db6754d57c663707124edc26c84593624
Andrey Bozhkov <kovbozh@gmail.com>
2022-02-28 10:53:25 +0300

    edit utils.py and variables.py

---
+++ lesson_7/common/utils.py
@@ -0,0 +1,41 @@
+import json
+import sys
+from common.variables import *
+
+sys.path.append('../')
+from errors import IncorrectDataRecivedError, NonDictInputError
+from decorators import log
+
+
+@log
+def get_message(client):
+    """
+    Утилита приёма и декодирования сообщения принимает байты и выдаёт словарь,
+    если принято что-то другое отдаёт ошибку значения.
+    :param client:
+    :return:
+    """
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
+
+
+@log
+def send_message(sock, message):
+    """
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
+    """
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
